c6399cda673178f13c82449b814e075466e26cf8
Mehdi <m.bamou@marostack.com>
2016-02-21 21:07:59 +0000

    create view and methods for top menu

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -14,36 +14,36 @@ class SiteController extends Controller
     public function behaviors()
     {
         return [
-            'access' => [
-                'class' => AccessControl::className(),
-                'only' => ['logout'],
-                'rules' => [
-                    [
-                        'actions' => ['logout'],
-                        'allow' => true,
-                        'roles' => ['@'],
-                    ],
-                ],
-            ],
-            'verbs' => [
-                'class' => VerbFilter::className(),
-                'actions' => [
-                    'logout' => ['post'],
-                ],
-            ],
+        'access' => [
+        'class' => AccessControl::className(),
+        'only' => ['logout'],
+        'rules' => [
+        [
+        'actions' => ['logout'],
+        'allow' => true,
+        'roles' => ['@'],
+        ],
+        ],
+        ],
+        'verbs' => [
+        'class' => VerbFilter::className(),
+        'actions' => [
+        'logout' => ['post'],
+        ],
+        ],
         ];
     }
 
     public function actions()
     {
         return [
-            'error' => [
-                'class' => 'yii\web\ErrorAction',
-            ],
-            'captcha' => [
-                'class' => 'yii\captcha\CaptchaAction',
-                'fixedVerifyCode' => YII_ENV_TEST ? 'testme' : null,
-            ],
+        'error' => [
+        'class' => 'yii\web\ErrorAction',
+        ],
+        'captcha' => [
+        'class' => 'yii\captcha\CaptchaAction',
+        'fixedVerifyCode' => YII_ENV_TEST ? 'testme' : null,
+        ],
         ];
     }
 
@@ -64,7 +64,7 @@ class SiteController extends Controller
         }
         return $this->render('login', [
             'model' => $model,
-        ]);
+            ]);
     }
 
     public function actionLogout()
@@ -84,7 +84,7 @@ class SiteController extends Controller
         }
         return $this->render('contact', [
             'model' => $model,
-        ]);
+            ]);
     }
 
     public function actionApropos()
@@ -92,20 +92,70 @@ class SiteController extends Controller
         return $this->render('apropos');
     }
 
+    public function actionEtudenalyse()
+    {
+        return $this->render('etudeanalyse');
+    }
+
+    public function actionPartener()
+    {
+        return $this->render('partener');
+    }
+
+    public function actionTechnologies()
+    {
+        return $this->render('technologies');
+    }
+
+    public function actionMaghribup()
+    {
+        return $this->render('maghribup');
+    }
+
+    public function actionMicrosite()
+    {
+        return $this->render('microsite');
+    }
+
+    public function actionPmestarter()
+    {
+        return $this->render('pmestarter');
+    }
+
+    public function actionSmsdaba()
+    {
+        return $this->render('smsdaba');
+    }
+
+    public function actionMaronethelp()
+    {
+        return $this->render('maronethelp');
+    }
+
+    public function actionNasna()
+    {
+        return $this->render('nasna');
+    }
+
+    public function actionNumerini()
+    {
+        return $this->render('numerini');
+    }
+
    /**
      * Ajax handler for language change dropdown list. Sets cookie ready for next request
      */
-    public function actionLanguage()
+   public function actionLanguage()
+   {
+    if ( Yii::$app->request->post('_lang') !== NULL && array_key_exists(Yii::$app->request->post('_lang'), Yii::$app->params['languages']))
     {
-        if ( Yii::$app->request->post('_lang') !== NULL && array_key_exists(Yii::$app->request->post('_lang'), Yii::$app->params['languages']))
-        {
-            Yii::$app->language = Yii::$app->request->post('_lang');
-            $cookie = new yii\web\Cookie([
+        Yii::$app->language = Yii::$app->request->post('_lang');
+        $cookie = new yii\web\Cookie([
             'name' => '_lang',
             'value' => Yii::$app->request->post('_lang'),
             ]);
-            Yii::$app->getResponse()->getCookies()->add($cookie);
-        }
-        Yii::$app->end();
+        Yii::$app->getResponse()->getCookies()->add($cookie);
     }
+    Yii::$app->end();
+}
 }
--- views/site/apropos.php
+++ views/site/apropos.php
@@ -10,14 +10,14 @@ $this->params['breadcrumbs'][] = $this->title;
 <div class="site-about">
 	<h1><?= Html::encode($this->title) ?></h1>
 
-	<p class="lead">
+	<p>
 		Dès sa création, Marostack s’est positionnée comme un des principaux acteurs de l’expertise des systèmes d’information en apportant des réponses et des solutions complétes aux besoins de ses clients.
 		<br />
 		Marostack accompagne chacun de ses clients en optimisant leurs ressources grâce à son cœur métier : l'utilisation combinée de moyens informatiques, électroniques et de procédés de télécommunication, dans une vision de mise en place et d'amélioration des systèmes d'information.
 		<br />
 		Ceci est formalisé au niveau de divers sections : 
 	</p>
-	<ul class="lead">
+	<ul>
 			<li>L’infogérance </li>
 			<li>Les Services informatiques</li>
 			<li>La sécurité des données et des infrastructures</li>
@@ -25,10 +25,10 @@ $this->params['breadcrumbs'][] = $this->title;
 			<li>Le Conseil et l’expertise</li>
 			<li>Le développement de solutions métiers.</li>
 		</ul>
-	<p class="lead">
+	<p>
 		Notre équipe d'ingénieurs et de techniciens vous apporte un service performant en collaboration avec les constructeurs et les éditeurs de logiciel et de technologies telque la Free Software Fondation - The GNU Projet.
 	</p>		
-	<p class="lead">
+	<p>
 		Plus qu’une simple Société de Service d’Ingénierie Informatique, Marostack a pour vocation de générer une pépinière de compétences autour d’un projet commun dont le but est la synergie des différents métiers complexes que regroupent les systèmes d’information afin d'accompagner ses clients vers un niveau amélioré d'efficacité et de productivité.
 	</p>
 </div>
