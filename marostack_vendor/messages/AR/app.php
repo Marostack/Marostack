4b544ad9c7c8773dcf3df54351863f4739029f81
Mehdi Bamou <m.bamou@marostack.com>
2016-12-15 20:31:21 +0000

    major update 2 - folder structure

--- marostack_vendor/messages/AR/app.php
+++
@@ -1,8 +0,0 @@
-<?php
-return [
-	'name' => 'اﻹسم',
-	'email' => 'البريد',
-	'subject' => 'الموضوع',
-	'body' => 'نص الرسالة',
-	'Verification Code' => 'كلمة التحقيق',
-];
--- marostack_vendor/messages/EN/app.php
+++
@@ -1,8 +0,0 @@
-<?php
-return [
-	'name' => 'Name',
-	'email' => 'Email',
-	'subject' => 'Subject',
-	'body' => 'Body',
-	'Verification Code' => 'Verification code',
-];
